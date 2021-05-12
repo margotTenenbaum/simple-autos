b93bee7bbb21630bfd8b2a48a9fa347a2cbf9d1f
Siyun Wang <wangsiyun0518@gmail.com>
2021-05-12 12:30:53 -0700

    set up integration tests

--- src/test/java/com/galvanize/autos/AutosApplicationTests.java
+++ src/test/java/com/galvanize/autos/AutosApplicationTests.java
@@ -1,13 +1,48 @@
 package com.galvanize.autos;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AutosApplicationTests {
+    @Autowired
+    TestRestTemplate testRestTemplate;
+    @Autowired
+    AutosRepository autosRepository;
 
-//	@Test
-//	void contextLoads() {
-//	}
+    List<Auto> testAutos;
+    @BeforeEach
+    void setup() {
+        testAutos = new ArrayList<>();
+        Auto auto;
+        String[] colors = {"red", "yellow", "blue", "green", "orange", "black", "white", "silver"};
+        String[] makes = {"Ford", "Tesla", "Honda", "Toyota", "Audi", "BMW", "Nissan", "Chevrolet"};
+        String[] models = {"Explorer", "Y", "Civic", "Corolla", "A3", "M3", "Altima", "Camaro"};
+        int[] years = {2001, 2003, 2012, 2014, 2015, 2019, 2020, 2021};
+        Random r = new Random();
+        for (int i = 0; i < 50; i++) {
+            int random = r.nextInt(8);
+            auto = new Auto(colors[r.nextInt(8)], makes[random], models[random], years[r.nextInt(8)], "AABBCC" + i);
+            testAutos.add(auto);
+        }
+        autosRepository.saveAll(testAutos);
+    }
+
+    @AfterEach
+    void tearDown() {
+        autosRepository.deleteAll();
+    }
+
+	@Test
+	void contextLoads() {
+	}
 
 }
