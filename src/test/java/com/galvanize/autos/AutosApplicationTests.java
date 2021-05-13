be28cca20bd3dbb3b7b03bef46e82886b48b570d
davidlira1 <liradavid100@gmail.com>
2021-05-13 09:32:07 -0700

    add patch integration tests

--- src/test/java/com/galvanize/autos/AutosApplicationTests.java
+++ src/test/java/com/galvanize/autos/AutosApplicationTests.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -113,5 +115,48 @@ class AutosApplicationTests {
         ResponseEntity<Auto> response = testRestTemplate.getForEntity("/api/autos/AABBCC51", Auto.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
         assertThat(response.getBody()).isNull();
+    }
+
+    @Test
+    void updateAuto_returnsUpdatedAuto() {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        testRestTemplate.getRestTemplate().setRequestFactory(requestFactory);
+        UpdateAuto updateAuto = new UpdateAuto("purple", "Elon");
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity<UpdateAuto> request = new HttpEntity<>(updateAuto, headers);
+
+        ResponseEntity<Auto> response = testRestTemplate.exchange("/api/autos/AABBCC1", HttpMethod.PATCH, request, Auto.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody().getOwner()).isEqualTo(updateAuto.getOwner());
+    }
+
+    @Test
+    void updateAuto_returnsNoContentForInvalidVIN() {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        testRestTemplate.getRestTemplate().setRequestFactory(requestFactory);
+        UpdateAuto updateAuto = new UpdateAuto("purple", "Elon");
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity<UpdateAuto> request = new HttpEntity<>(updateAuto, headers);
+
+        ResponseEntity<Auto> response = testRestTemplate.exchange("/api/autos/AABBCC51", HttpMethod.PATCH, request, Auto.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
+    @Test
+    void updateAuto_returnsBadRequestForInvalidBody() {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        testRestTemplate.getRestTemplate().setRequestFactory(requestFactory);
+//        UpdateAuto updateAuto = new UpdateAuto("purple");
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity<Object> request = new HttpEntity<>("anything", headers);
+
+        ResponseEntity<Auto> response = testRestTemplate.exchange("/api/autos/AABBCC1", HttpMethod.PATCH, request, Auto.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
     }
 }
