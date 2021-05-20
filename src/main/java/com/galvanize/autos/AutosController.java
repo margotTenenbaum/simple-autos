a7148f4ea2bb410452b5ad04709e0fb19820ef33
davidlira1 <liradavid100@gmail.com>
2021-05-20 10:02:52 -0700

    removed parens

--- src/main/java/com/galvanize/autos/AutosController.java
+++ src/main/java/com/galvanize/autos/AutosController.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("*")
+@CrossOrigin
 @RestController
 @RequestMapping("/api/autos")
 public class AutosController {
